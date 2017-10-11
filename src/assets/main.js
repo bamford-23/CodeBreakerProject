14528b79e559f68635530b622de170fa6f8e3986
AlexBamford <alex.bamford@import.io>
2017-10-11 11:18:16 +0100

    loose change

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,21 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
+    if (answer == "" || attempt == ""){
+      setHiddenFields();
+    };
+
+
     //add functionality to guess function here
+
 }
 
 //implement new functions here
+function setHiddenFields(){
+  attempt = 0
+  let val = ""
+  for(i=0; i<=4;1++){
+    val += (Math.floor(Math.random()*10)).toString();
+  answer.value = Number(val);
+  };
+}
