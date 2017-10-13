f8d8aa3b375d4ef5dddd4fd330d3a624c4f6d2a9
Alex Bamford <alex.bamford@import.io>
2017-10-13 09:27:01 +0100

    submission

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,34 +1,15 @@
 
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
-let count = 0
-function guess(){
-  let para = ""
-  let input = document.getElementById('user-guess').value;
 
-  if (answer.value == "" || attempt.value == ""){
-    setHiddenFields();
-  }
 
-  if((validateInput(input)) == true){
-    console.log('fuck you');
-    attempt.value ++;
-  }
+function setMessage(para){
+  let message = document.getElementById('message');
+  message.innerHTML = para;
+}
 
-  if (count !== 4 && attempt.value >= 10){
-    setMessage("You Lose! :(");
-
-  }else if(count !== 4){
-    setMessage("Incorrect, try again.");
-
-  }else{
-    setMessage("You Win! :)");
-  }
-  console.log(count);
-  getResults(input);
-//implement new functions here
 function setHiddenFields(){
-  attempt.value = 0
+  attempt.value = 0;
 
   let val = ""
   for(i=0; i<4;i++){
@@ -45,19 +26,10 @@ function validateInput(input){
   }else
     setMessage("");
     return true;
-  };
-
-
-
-function setMessage(para){
-  let message = document.getElementById('message');
-  message.innerHTML = para;
 }
 
-
-}
 function getResults(input){
-
+  let correct = 0;
   let result = document.getElementById('results');
 
   let html = '<div class="row"><span class="col-md-6">' + input + '</span><span class="col-md-6"</span>';
@@ -66,19 +38,62 @@ function getResults(input){
 
     if (input.charAt(i) == answer.value.charAt(i)){
       html += '<span class="glyphicon glyphicon-ok"></span>';
-      count ++;
+      correct ++;
 
+    }else if (answer.value.indexOf(input.charAt(i)) > -1){
+      html += '<span class="glyphicon glyphicon-transfer"></span>';
     }else{
-      for (n=0; n < input.length; n++){
-        if (input.charAt(i) == answer.value.charAt(n)){
-          html += '<span class="glyphicon glyphicon-transfer"></span>';
-        }else{
-          html += '<span class="glyphicon glyphicon-remove">';
-        }
-      }
+      html += '<span class="glyphicon glyphicon-remove">';
     }
+  }
   html += ''
+  result.innerHTML += html;
+  if(correct == 4){
+    setMessage("You Win! :)");
+    showAnswer(true);
+    showReplay();
 
   }
-  result.innerHTML += html;
+  else if (correct !== 4 && attempt.value > 10){
+    setMessage("You Lose! :(");
+    showAnswer(false);
+    showReplay();
+
+  }else if(correct !== 4){
+    setMessage("Incorrect, try again.");
+  }
+}
+
+
+function guess(){
+
+  let para = ""
+  let input = document.getElementById('user-guess').value;
+  if (answer.value == "" || attempt.value == ""){
+    setHiddenFields();
+  }
+
+  if((validateInput(input)) == true){
+
+    attempt.value ++;
+  }else{
+    return false;
+  }
+  getResults(input);
+
+}
+
+function showAnswer(success){
+  if (success){
+    code.className += " success";
+  }else{
+    code.className += " failure";
+  }
+
+  document.getElementById('code').innerHTML = answer.value;
+}
+
+function showReplay(){
+  document.getElementById('guessing-div').style.display = 'none';
+  document.getElementById('replay-div').style.display = 'block';
 }
